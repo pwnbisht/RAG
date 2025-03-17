ea055fd85a156ade97f2ca31462cfdd52723b035
Pawan Bisht <35245223+pwnbisht@users.noreply.github.com>
2025-03-17 13:45:30 +0530

    Delete frontend/.next directory

--- frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/media/pawan/a2034fcd-d514-4923-9d18-a504daa8b2f0/voiceai/frontend/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
